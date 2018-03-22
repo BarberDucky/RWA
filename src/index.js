004c320645c73ab8426b5ea11b22bba896d46fc4
unknown <damjan.trifunovic@gmail.com>
2018-03-22 11:13:10 +0100

    promise i fetch

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import {
     Movie
 } from "./movie";
 import { MovieService } from "./movies.service";
+import {MoviesService} from "../another-movies.service.js"
 
-const service = new MovieService();
-const movies = service.get(movies => {
+MoviesService.get().then(movies => console.log(movies))
+
+/*service.get().then(movies => { 
     movies.forEach(movie => ShowMovie(movie));
-});
-
-//ShowMovie(movies[1]);
+});*/
 
 function ShowMovie(movie){
     const {title, year, score} = movie;
     document.write(`<p>${title}, ${year}, ${score}</p>`);
 }
 
+/*service.retRand()  // promise u promise-u
+    .then(num => service.get(num)
+    .then(movie => console.log(movie)))*/
+
+/*Promise.all([ // promise koji paralelno pozove funkcije i po zavrsetku svih prelazi u then
+    service.get(),
+    service.retRand()
+]).then(([movies, number]) => { // argument then-a je vektor sa povratnim vrednostima gorepozvanih promise-a
+    console.log(`there are ${number} movies: `)
+    console.log(movies)
+})*/
--- src/movies.service.js
+++ src/movies.service.js
@@ -8,8 +8,26 @@ export class MovieService {
         this.movies.push(new Movie("Expendables", 2015, 9));
         this.movies.push(new Movie("Mrak film", 2005, 2));
     }
-    get(cbFunc){
-        setTimeout(() => cbFunc(this.movies), 2000);
-        //return this.movies;
+
+    getCb(cbf){ // get sa callbackom
+        setTimeout(() => cbf(movies), 1000)
+    }
+
+    get(i){ // vraca element sa indexom i
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(this.movies[i]), 1000)
+        })
+    }
+
+    get(){ // vraca ceo niz
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(this.movies), 1000)
+        })
+    }
+
+    retRand(){ // vraca random broj preko promisa
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(Math.floor(Math.random() * 4)), 1000)
+        })
     }
 }
